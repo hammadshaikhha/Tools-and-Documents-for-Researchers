068c47e01c85e9c82e5feaaf64fc55930f8c05dd
Hammad Shaikh <hammy.shaikh@mail.utoronto.ca>
2019-03-24 02:02:36 -0400

    Includes 95% student t CI

--- Version Control Using Git/BootstrapCI.py
+++ Version Control Using Git/BootstrapCI.py
@@ -6,17 +6,34 @@ is small and population is right skewed.
 # Load packages
 import numpy as np
 import seaborn
+from scipy import stats
 import matplotlib.pyplot as plt
 
 # Initialize parameters
 shape_a = 1.5
 shape_b = 5
+alpha = 0.05
 n_sample = 20
+df = n_sample-1
 
 # Right skewed Population distribution
 X = np.random.beta(shape_a, shape_b, 5000)
 
-# Histogram of population
-seaborn.distplot(X)
-plt.title("Right Skewed Population Distribution")
-plt.show()
+# Distribution of population
+#seaborn.distplot(X)
+#plt.title("Right Skewed Population Distribution")
+#plt.show()
+
+# Draw a sample from Population
+data = np.random.beta(shape_a, shape_b, n_sample)
+
+## Standard 95% CI
+# CI components
+sample_mean = np.mean(data)
+sample_std = np.std(data)
+margin_error = stats.t.ppf(1-alpha/2, df)*(sample_std/np.sqrt(n_sample))
+
+# Lower and upper bounds
+CI_lower = sample_mean - margin_error
+CI_upper = sample_mean + margin_error
+print("The 95% CI is: [" + str(CI_lower) + "," + str(CI_upper) + "]")
