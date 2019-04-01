a6e9cc6bd8d6da25c8fba61abe186c38065219fe
Hammad Shaikh <hammy.shaikh@mail.utoronto.ca>
2019-03-31 23:07:46 -0400

    Generate two samples from different beta distributions

---
+++ Version Control Using Git/StatisticalPower.R
@@ -0,0 +1,12 @@
+# Purpose: Compute power of hypothesis testing
+# for difference between two means using both
+# assymptotic and exact tests.
+# Data Started: Sunday, March 31, 2019
+
+# Sample sizes
+n1 = 15
+n2 = 20
+
+# Generate samples
+X1 = rbeta(n1, 2, 5)
+X2 = rbeta(n2, 3, 3)
