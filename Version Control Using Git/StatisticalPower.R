3864226c902e3f73cdbb3f374d69f25dc53b3ad7
Hammad Shaikh <hammy.shaikh@mail.utoronto.ca>
2019-04-01 00:15:59 -0400

    Conduct two means t-test

--- Version Control Using Git/StatisticalPower.R
+++ Version Control Using Git/StatisticalPower.R
@@ -6,7 +6,22 @@
 # Sample sizes
 n1 = 15
 n2 = 20
+alpha = 0.05
 
 # Generate samples
 X1 = rbeta(n1, 2, 5)
 X2 = rbeta(n2, 3, 3)
+
+# Degrees of freedom
+df = n1 + n2 - 2
+
+# Pooled variance
+pool_var = ((n1-1)*var(X1) + (n2-1)*var(X2))/(n1 + n2 - 2)
+
+# t - statistics and critical vaue
+tstat = (mean(X2) - mean(X1))/sqrt(pool_var*(1/n1 + 1/n2))
+tcv = abs(qt(alpha/2, df))
+
+# Rejection region
+reject = tstat > tcv
+tcv
